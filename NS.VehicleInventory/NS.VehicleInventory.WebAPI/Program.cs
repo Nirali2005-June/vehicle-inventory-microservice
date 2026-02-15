6dc08c8cba31252d94cfa1309edf07ba5c67b46f
Nirali999 <vasantisathvara0@gmail.com>
2026-02-15 18:58:48 -0500

    Added SQL Server connection string for VehicleInventoryDb

--- NS.VehicleInventory/NS.VehicleInventory.WebAPI/Program.cs
+++ NS.VehicleInventory/NS.VehicleInventory.WebAPI/Program.cs
@@ -1,4 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using NS.VehicleInventory.Application.Interfaces;
+using NS.VehicleInventory.Application.Services;
+using NS.VehicleInventory.Infrastructure.Data;
+using NS.VehicleInventory.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<VehicleInventoryDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("VehicleInventoryConnection")));
+
+builder.Services.AddScoped<IVehicleRepository, VehicleRepository>();
+builder.Services.AddScoped<VehicleService>();
+
 
 // Add services to the container.
 
