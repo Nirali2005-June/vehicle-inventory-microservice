61da0e9154426999c53dcd867056d84f785b85c5
Nirali999 <vasantisathvara0@gmail.com>
2026-02-15 17:33:48 -0500

    Added IVehicleRepository abstraction to Application layer

---
+++ NS.VehicleInventory/NS.VehicleInventory.Application/Interfaces/IVehicleRepository.cs
@@ -0,0 +1,12 @@
+﻿using VehicleInventory.Domain.Entities;
+
+namespace NS.VehicleInventory.Application.Interfaces;
+
+public interface IVehicleRepository
+{
+    Task AddAsync(Vehicle vehicle);
+    Task<Vehicle?> GetByIdAsync(Guid id);
+    Task<List<Vehicle>> GetAllAsync();
+    Task UpdateAsync(Vehicle vehicle);
+    Task DeleteAsync(Vehicle vehicle);
+}
