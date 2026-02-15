ebcad4fcd833e17a1d45f7e5e9540c31f8706273
Nirali999 <vasantisathvara0@gmail.com>
2026-02-15 18:06:50 -0500

    Implemented DbContext and VehicleRepository in Infrastructure layer

---
+++ NS.VehicleInventory/NS.VehicleInventory.Infrastructure/Data/VehicleInventoryDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VehicleInventory.Domain.Entities;
+
+namespace NS.VehicleInventory.Infrastructure.Data;
+
+public class VehicleInventoryDbContext : DbContext
+{
+    public VehicleInventoryDbContext(
+        DbContextOptions<VehicleInventoryDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Vehicle> Vehicles => Set<Vehicle>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Vehicle>(entity =>
+        {
+            entity.HasKey(v => v.Id);
+
+            entity.Property(v => v.VehicleCode)
+                  .IsRequired()
+                  .HasMaxLength(50);
+
+            entity.Property(v => v.LocationId)
+                  .IsRequired();
+
+            entity.Property(v => v.VehicleType)
+                  .IsRequired();
+
+            entity.Property(v => v.Status)
+                  .IsRequired();
+
+            entity.HasIndex(v => v.VehicleCode)
+                  .IsUnique();
+        });
+    }
+}
---
+++ NS.VehicleInventory/NS.VehicleInventory.Infrastructure/Repositories/VehicleRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NS.VehicleInventory.Application.Interfaces;
+using NS.VehicleInventory.Infrastructure.Data;
+using VehicleInventory.Domain.Entities;
+
+namespace NS.VehicleInventory.Infrastructure.Repositories;
+
+public class VehicleRepository : IVehicleRepository
+{
+    private readonly VehicleInventoryDbContext _context;
+
+    public VehicleRepository(VehicleInventoryDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(Vehicle vehicle)
+    {
+        await _context.Vehicles.AddAsync(vehicle);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<List<Vehicle>> GetAllAsync()
+    {
+        return await _context.Vehicles.ToListAsync();
+    }
+
+    public async Task<Vehicle?> GetByIdAsync(Guid id)
+    {
+        return await _context.Vehicles
+            .FirstOrDefaultAsync(v => v.Id == id);
+    }
+
+    public async Task UpdateAsync(Vehicle vehicle)
+    {
+        _context.Vehicles.Update(vehicle);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Vehicle vehicle)
+    {
+        _context.Vehicles.Remove(vehicle);
+        await _context.SaveChangesAsync();
+    }
+}
