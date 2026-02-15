13dd9dc88928e6cd7fa5dbacccd4293999e985db
Nirali999 <vasantisathvara0@gmail.com>
2026-02-15 16:12:17 -0500

    Added VehicleStatus enum to represent vehicle lifecycle states

---
+++ NS.VehicleInventory/VehicleInventory.Domain/Enums/VehicleStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleInventory.Domain.Enums;
+
+public enum VehicleStatus
+{
+    Available = 1,
+    Rented = 2,
+    Reserved = 3,
+    InService = 4
+}
