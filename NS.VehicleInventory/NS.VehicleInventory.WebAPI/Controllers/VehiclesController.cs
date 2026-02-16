f30905d1314f9b8777e74db43f475be54fd9fd19
Nirali999 <vasantisathvara0@gmail.com>
2026-02-15 19:21:51 -0500

    Created VehiclesController with GetAll and Create endpoints

---
+++ NS.VehicleInventory/NS.VehicleInventory.WebAPI/Controllers/VehiclesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NS.VehicleInventory.Application.Services;
+using NS.VehicleInventory.Application.DTOs;
+
+namespace NS.VehicleInventory.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class VehiclesController : ControllerBase
+{
+    private readonly VehicleService _vehicleService;
+
+    public VehiclesController(VehicleService vehicleService)
+    {
+        _vehicleService = vehicleService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var vehicles = await _vehicleService.GetAllVehiclesAsync();
+        return Ok(vehicles);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateVehicleRequest request)
+    {
+        var result = await _vehicleService.CreateVehicleAsync(request);
+        return Ok(result);
+    }
+}
