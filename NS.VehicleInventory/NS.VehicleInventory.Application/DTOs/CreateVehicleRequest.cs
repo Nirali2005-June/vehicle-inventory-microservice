83ea0129a73de93f6c01b06397a5e26c380c73b4
Nirali999 <vasantisathvara0@gmail.com>
2026-02-16 21:17:47 -0500

    Implemented input validation for CreateVehicleRequest using DataAnnotations

--- NS.VehicleInventory/NS.VehicleInventory.Application/DTOs/CreateVehicleRequest.cs
+++ NS.VehicleInventory/NS.VehicleInventory.Application/DTOs/CreateVehicleRequest.cs
@@ -1,7 +1,11 @@
-﻿namespace NS.VehicleInventory.Application.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NS.VehicleInventory.Application.DTOs;
 
 public class CreateVehicleRequest
 {
+    [Required]
+    [MaxLength(50)]
     public string VehicleCode { get; set; } = string.Empty;
     public string LocationId { get; set; } = string.Empty;
     public string VehicleType { get; set; } = string.Empty;
